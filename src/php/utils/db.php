e06b54502c418ab5c1bcb40a89cf2627f686a6bf
Michał Broś <63966121+MBrosik@users.noreply.github.com>
2023-03-26 22:26:30 +0200

    Update db.php

--- src/php/utils/db.php
+++ src/php/utils/db.php
@@ -1,10 +1,5 @@
 <?php
 
-// $host = "mysql.ct8.pl";
-// $user = "m22177_mbros";
-// $passwd = "yWxnbNygQlNe5G2ksu3f";
-// $dbname = "m22177_cinema";
-
 $host = "localhost";
 $user = "root";
 $passwd = "";
